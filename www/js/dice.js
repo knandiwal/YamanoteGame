dfa7947fe295788822f653fab7c381687242c788
itigoseiyaku <itigoseiyaku@yahoo.co.jp>
2012-11-17 15:03:48 +0900

    commit
    test

---
+++ www/js/dice.js
@@ -0,0 +1,11 @@
+﻿var state=false;
+//state 
+//true Rotation now
+//false Rotetion stop
+function dice_select(){
+	if(state==true){dice_submit();}
+	else {state=true;}
+}
+function dice_submit(){
+	Rnd
+}
